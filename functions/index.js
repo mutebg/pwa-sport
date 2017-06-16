4be0a2077b014fa17dcc60d3e30ec2a7c2abe2f8
Stoyan Delev <s.delev@elements.nl>
2017-06-17 00:13:42 +0200

    add fb func to work with strava

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,46 @@
 const functions = require('firebase-functions');
+const strava = require('strava-v3');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.stravaCallback = functions.https.onRequest((request, response) => {
+	//console.log(request);
+	const code = request.query.code;
+	const state = request.query.state;
+	strava.oauth.getToken(code, (err, stravaResp) => {
+		if (!err) {
+			response.json({
+				code,
+				state,
+				token: stravaResp.access_token
+			});
+		}
+		else {
+			response.json(err);
+		}
+	});
+});
+
+exports.stravaLogin = functions.https.onRequest((request, response) => {
+	const url = strava.oauth.getRequestAccessURL({
+		scope: 'view_private,write'
+	});
+	response.redirect(url);
+});
+
+exports.stravaUpload = functions.https.onRequest((request, response) => {
+	const token = request.body.token;
+	const record = request.body.record;
+	strava.uploads.post(
+		{
+			access_token: token,
+			data_type: 'gpx',
+			file: 'data/your_file.gpx',
+			name: 'Epic times',
+			statusCallback(err, payload) {
+				//do something with your payload
+			}
+		},
+		(err, payload, limits) => {
+			//do something with your payload, track rate limits
+		}
+	);
+});
