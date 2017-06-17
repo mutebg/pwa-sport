d51fc11de0fac29ade42465c6455d17bd073546e
Stoyan Delev <s.delev@elements.nl>
2017-06-17 11:54:57 +0200

    create xml track

--- functions/index.js
+++ functions/index.js
@@ -29,18 +29,56 @@ exports.stravaLogin = functions.https.onRequest((request, response) => {
 exports.stravaUpload = functions.https.onRequest((request, response) => {
 	const token = request.body.token;
 	const record = request.body.record;
+
 	strava.uploads.post(
 		{
-			access_token: token,
+			//access_token: token,
 			data_type: 'gpx',
-			file: 'data/your_file.gpx',
-			name: 'Epic times',
+			//file: 'data/your_file.gpx',
+			file: generateTrack(record),
+			name: generateName(record),
 			statusCallback(err, payload) {
+				console.log(payload);
 				//do something with your payload
 			}
 		},
 		(err, payload, limits) => {
+			console.log({ err });
 			//do something with your payload, track rate limits
+			response.json(payload);
 		}
 	);
+	console.log('end of Func');
 });
+
+const generateName = record =>
+	`Run on ${new Date(record[0].time).toLocaleDateString()}`;
+
+const generateTrack = record => {
+	const points = record.map(
+		({ gps, heartRate, time }) => `
+		<trkpt lat="${gps.lat}" lon="${gps.lng}">
+    <ele>0</ele>
+    <time>${new Date(time).toISOString()}</time>
+    <extensions>
+     <gpxtpx:TrackPointExtension>
+      <gpxtpx:hr>${heartRate}</gpxtpx:hr>
+     </gpxtpx:TrackPointExtension>
+    </extensions>
+   </trkpt>
+	`
+	);
+
+	return `
+<?xml version="1.0" encoding="UTF-8"?>
+<gpx creator="StravaGPX" version="1.1" xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd" xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1" xmlns:gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3">
+	<metadata>
+  	<time>${new Date(record[0].time).toISOString()}</time>
+ 	</metadata>
+ 	<trk>
+  	<name>${generateName(record)}</name>
+  	<trkseg>${points.join('')}</trkseg>
+	</trk>
+</gpx>
+	`;
+};
