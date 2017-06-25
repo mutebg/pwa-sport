0ee00917932204368c171fab640324dd60059906
Stoyan Delev <s.delev@elements.nl>
2017-06-25 10:02:07 +0200

    Combine all svgs in one file and remove other

--- README.md
+++ README.md
@@ -9,34 +9,13 @@ PWA Sport App which can track your heart rate and geo location  during exercise
 - Voice on every KM
 
 
-
 ## TODO
-- Feature detection for BT and Geo ( show error message )
-- Place all svgs in one file
 
 
-## UI
-- Design/Interface
-  - Home - default
-    - find gps
-    - find HR sensor
-    - button start after above two are found
-  - Home - running
-    - Speed
-    - Time
-    - Heart rate
-    - Distance
-    - Start / Stop button
-  - List of workouts
-    - Sync with strava
-    - Date
-    - Time
-    - Distance
-    - Av speed
-  - List details
-    - Data from list +
-    - Map
-    - Grapshs - Speed / HR /
+- List details
+  - Data
+  - Map
+  - Grapshs - Speed / HR /
 
 
 - https://dribbble.com/shots/3152567-Running-App-Concept/attachments/668803
--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -1,64 +1,16 @@
 import { h, Component } from 'preact';
-import heart from '../../assets/heart.svg';
-import location from '../../assets/location.svg';
-import run from '../../assets/run.svg';
-import time from '../../assets/time.svg';
-import timer from '../../assets/timer.svg';
-import start from '../../assets/start.svg';
-import pause from '../../assets/pause.svg';
-import date from '../../assets/date.svg';
-import fire from '../../assets/fire.svg';
 import style from './style.less';
 
 export default class Icon extends Component {
 	render({ name, size = '24px', pulse = false }) {
-		let svg = null;
-		switch (name) {
-			case 'heart':
-				svg = heart;
-				break;
-
-			case 'location':
-				svg = location;
-				break;
-
-			case 'run':
-				svg = run;
-				break;
-
-			case 'timer':
-				svg = timer;
-				break;
-
-			case 'time':
-				svg = time;
-				break;
-
-			case 'start':
-				svg = start;
-				break;
-
-			case 'pause':
-				svg = pause;
-				break;
-
-			case 'date':
-				svg = date;
-				break;
-
-			case 'fire':
-				svg = fire;
-				break;
-		}
-
-		const className = pulse ? style.pulse : null;
-
+		const className = pulse ? style.pulse + ' icon' : 'icon';
 		return (
-			<img
-				src={svg}
+			<svg
 				class={className}
 				style={{ verticalAlign: 'middle', width: size, height: size }}
-			/>
+			>
+				<use xlinkHref={`#${name}`} />
+			</svg>
 		);
 	}
 }
