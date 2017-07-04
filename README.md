b755216911ff78fe0400a895960b0fff9467a6a2
Stoyan Delev <s.delev@elements.nl>
2017-07-04 22:42:32 +0200

    update preact, fix svg color

--- README.md
+++ README.md
@@ -4,12 +4,17 @@ PWA Sport App which can track your heart rate and geo location  during exercise
 
 ## DONE
 - Connect to BT device and take heart_rate
-- Use GEO loation api to take current position
+- Use GEO location api to take current position
 - Sync with Strava - use firebase functions and https://www.npmjs.com/package/strava-v3
 - Voice on every KM
 
 
 ## TODO
+- Add animations / transitions
+- Feature detection for iOS
+- Permission api to handle rejected permissions
+- Google maps - dynamic imports ?!
+
 
 
 - List details
--- src/components/app.js
+++ src/components/app.js
@@ -55,7 +55,6 @@ export default class App extends Component {
 							fill="url(#gr)"
 							d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"
 						/>
-
 					</symbol>
 					<symbol id="location" viewBox="0 0 24 24">
 						<path d="M0 0h24v24H0z" fill="none" />
@@ -64,8 +63,8 @@ export default class App extends Component {
 							d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V1h-2v2.06C6.83 3.52 3.52 6.83 3.06 11H1v2h2.06c.46 4.17 3.77 7.48 7.94 7.94V23h2v-2.06c4.17-.46 7.48-3.77 7.94-7.94H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"
 						/>
 					</symbol>
-					<symbol id="pause" viewBox="0 0 24 24" fill="#000">
-						<path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
+					<symbol id="pause" viewBox="0 0 24 24">
+						<path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" fill="#fff" />
 						<path d="M0 0h24v24H0z" fill="none" />
 					</symbol>
 					<symbol id="run" viewBox="0 0 24 24">
@@ -75,8 +74,8 @@ export default class App extends Component {
 							d="M13.49 5.48c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm-3.6 13.9l1-4.4 2.1 2v6h2v-7.5l-2.1-2 .6-3c1.3 1.5 3.3 2.5 5.5 2.5v-2c-1.9 0-3.5-1-4.3-2.4l-1-1.6c-.4-.6-1-1-1.7-1-.3 0-.5.1-.8.1l-5.2 2.2v4.7h2v-3.4l1.8-.7-1.6 8.1-4.9-1-.4 2 7 1.4z"
 						/>
 					</symbol>
-					<symbol id="start" viewBox="0 0 24 24" fill="#000">
-						<path d="M8 5v14l11-7z" />
+					<symbol id="start" viewBox="0 0 24 24">
+						<path d="M8 5v14l11-7z" fill="#fff" />
 						<path d="M0 0h24v24H0z" fill="none" />
 					</symbol>
 					<symbol id="time" viewBox="0 0 24 24">
@@ -85,7 +84,6 @@ export default class App extends Component {
 							fill="url(#gr)"
 							d="M16.24 7.76C15.07 6.59 13.54 6 12 6v6l-4.24 4.24c2.34 2.34 6.14 2.34 8.49 0 2.34-2.34 2.34-6.14-.01-8.48zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"
 						/>
-
 					</symbol>
 					<symbol id="timer" viewBox="0 0 24 24">
 						<path d="M0 0h24v24H0z" fill="none" />
@@ -95,7 +93,6 @@ export default class App extends Component {
 						/>
 					</symbol>
 				</svg>
-
 			</div>
 		);
 	}
